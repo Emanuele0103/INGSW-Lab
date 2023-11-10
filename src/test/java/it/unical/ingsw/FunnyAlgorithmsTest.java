567883f5e045b496cf9d6d3892ccb21335eb40fc
Emanuele <emanuelegrande2@gmail.com>
2023-11-10 08:28:53 +0100

    Add Before and After Annotations

--- src/test/java/it/unical/ingsw/FunnyAlgorithmsTest.java
+++ src/test/java/it/unical/ingsw/FunnyAlgorithmsTest.java
@@ -1,5 +1,29 @@
 package it.unical.ingsw;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 public class FunnyAlgorithmsTest {
+    private static FunnyAlgorithms f;
+
+    @BeforeAll
+    public static void beforeAll(){ System.out.println("Before Class"); }
+
+    @AfterAll
+    public static void afterClass(){ System.out.println("After Class"); }
+
+    @BeforeEach
+    public void startTest(){
+        System.out.println("Start Test!");
+        f = new FunnyAlgorithms();
+    }
+
+    @AfterEach
+    public void finishTest(){
+        System.out.println("Finish Test!");
+    }
+
 
 }
